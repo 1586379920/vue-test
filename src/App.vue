c79f8b3858b9ca15c438fad0d9cde51d2b49749e
wang_jiaxi <1586379920@qq.com>
2022-06-24 09:30:16 +0800

    添加成功

---
+++ src/App.vue
@@ -0,0 +1,25 @@
+<template>
+  <h1>{{data.name}}</h1>
+  <button @click="changeNumber">an</button>
+</template>
+
+<script>
+import { reactive } from "vue"
+
+export default {
+  name: 'App',
+  setup(){
+    let data=reactive({
+      name:'jungle'
+    })
+    function changeNumber(){
+      console.log(data)
+    }
+    return {
+      data,
+      changeNumber
+    }
+  }
+}
+</script>
+
